724a19f84e202cea743651d4789f9d4e188ec2dd
isabellaisaxsson <isabella.isaksson2@outlook.com>
2024-02-20 16:47:01 +0100

    Question1 and 2

--- ArrayProjectAssignment/ArrayProjectAssignment/Program.cs
+++ ArrayProjectAssignment/ArrayProjectAssignment/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] inputArray = { "renault", "volkswagen", "audi", "volvo", "dacia", "alfa Romeo" };
+        List<string> filteredList = FilterString(inputArray);
+
+        Console.WriteLine("Element som börjar på A:");
+        foreach (string item in filteredList)
+        {
+            Console.WriteLine(item);
+        }
+
+        string directoryPath = Path.Combine(Environment.CurrentDirectory, "A_Directory");
+        Directory.CreateDirectory(directoryPath);
+
+        string filePath = Path.Combine(directoryPath, "a_list.txt");
+        File.WriteAllLines(filePath, filteredList);
+
+        Console.WriteLine($"Filen har sparats i {filePath}");
+    }
+
+    static List<string> FilterString(string[] inputArray)
+    {
+        List<string> resultList = new List<string>();
+
+        foreach (string item in inputArray)
+        {
+            if (item.StartsWith("a", StringComparison.OrdinalIgnoreCase)) // Ignorera skillnader mellan små och stora bokstäver
+            {
+                resultList.Add(item);
+            }
+        }
+
+        return resultList;
+    }
+
+    }
